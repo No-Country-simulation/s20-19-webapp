2dfc0075c01feae2718a55d6889e8acf761c3d03
lizrz98 <lizbeth.zarazua104b@gmail.com>
2025-01-30 15:32:40 -0600

    Agregando boton

--- frontend/.next/server/webpack-runtime.js
+++ frontend/.next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("7c055f8a7dca83e3")
+/******/ 		__webpack_require__.h = () => ("a7c1bff63a32b9c4")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
